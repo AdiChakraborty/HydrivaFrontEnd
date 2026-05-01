d09dbf8f900aa1138253b722c2a9e496b12679af
Indranil Kundu <indranilkundu6@gmail.com>
2026-05-01 10:52:11 +0530

    logos added

--- src/lib/axiosInstance.js
+++ src/lib/axiosInstance.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://ecommerce-backend-solution-vercel.vercel.app/api",
-  // baseURL: "http://localhost:5000/api",
+  baseURL: "https://api.hydrivaofficial.com/api",
 });
 
 axiosInstance.interceptors.request.use(
